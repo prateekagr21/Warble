adde12bbc63e05a482b313cbf9d298bab18995f2
Prateek Agrawal <prateek.agr21@gmail.com>
2020-12-22 20:14:35 +0530

    Added permissions, App ICON

--- app/src/main/java/com/example/warble/HomeActivity.java
+++ app/src/main/java/com/example/warble/HomeActivity.java
@@ -1,16 +1,23 @@
 package com.example.warble;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,6 +33,7 @@ public class HomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Button button;
     private TextView textView;
+    private int PERMISSION_CODE = 77;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,7 +46,7 @@ public class HomeActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                getAllSongs();
+                requestPermission();
             }
         });
     }
@@ -61,5 +69,30 @@ public class HomeActivity extends AppCompatActivity {
         AllSongsAdapter allSongsAdapter = new AllSongsAdapter(audioList);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
         recyclerView.setAdapter(allSongsAdapter);
+    }
+
+    public void requestPermission(){
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    HomeActivity.this,
+                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PERMISSION_CODE
+                    );
+        }
+        else {
+            getAllSongs();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==PERMISSION_CODE){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                getAllSongs();
+            }
+            else {
+                Toast.makeText(context, "Storage Permission Denied", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
